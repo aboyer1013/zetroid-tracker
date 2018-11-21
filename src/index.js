26b3c0ce458b3bb21917cb2f2dd06f6feb01ac44
Aaron Boyer <aboyer1013@gmail.com>
2018-11-21 15:06:14 -0600

    Initial commit create react app

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const name = observable({
-    name: 'aaron',
-    change: action(() => {
-        name.name = 'kevin';
-    })
-});
-const App = observer((props) => {
-    return (
-        <div>
-            <button onClick={props.name.change}>Change</button>
-            <h1>ohai {props.name.name}</h1>
-            </div>
-    );
-});
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App name={name} />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
