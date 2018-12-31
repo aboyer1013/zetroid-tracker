e442881b3ea8600875768f9003bd49c0f14281df
aboyer <aboyer1013@gmail.com>
2018-12-30 21:29:56 -0600

    Code cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,55 @@
-// const path = require('path');
-//
-// module.exports = {
-// 	env: {
-// 		browser: true
-// 	},
-// 	extends: 'airbnb',
-// 	globals: {
-// 		it: false,
-// 	},
-// 	parser: 'babel-eslint',
-// 	rules: {
-// 		'arrow-body-style': 'off',
-// 		'class-methods-use-this': 'off',
-// 		'import/prefer-default-export': 'off',
-// 		'import/no-cycle': 'off',
-// 		'import/named': 'off',
-// 		'import/no-named-as-default': 'off',
-// 		'import/no-named-as-default-member': 'off',
-// 		'indent': ['error', 'tab'],
-// 		'jsx-a11y/anchor-is-valid': 'off',
-// 		'jsx-a11y/label-has-associated-control': 'off',
-// 		'jsx-a11y/label-has-for': 'off',
-// 		'jsx-a11y/no-static-element-interactions': 'off',
-// 		'jsx-a11y/click-events-have-key-events': 'off',
-// 		'max-len': 'off',
-// 		'no-console': 'off',
-// 		'no-new': 'off',
-// 		'no-param-reassign': 'off',
-// 		'no-restricted-syntax': 'off',
-// 		'no-tabs': 'off',
-// 		'no-use-before-define': 'off',
-// 		'react/destructuring-assignment': 'off',
-// 		'react/jsx-indent': [true, 'tab'],
-// 		'react/jsx-indent-props': [2, 'tab'],
-// 		'react/jsx-one-expression-per-line': 'off',
-// 		'react/jsx-filename-extension': 'off',
-// 		'react/no-access-state-in-setstate': 'off',
-// 		'react/prop-types': 'off',
-// 		'react/react-in-jsx-scope': 'off',
-// 	},
-// 	settings: {
-// 		'import/resolver': {
-// 			node: {
-// 				paths: [path.resolve(__dirname, 'src')],
-// 			},
-// 			// alias: {
-// 			// 	map: [
-// 			// 		['', './src'],
-// 			// 	],
-// 			// 	extensions: ['.js', '.jsx'],
-// 			// }
-// 		}
-// 	}
-// };
+const path = require('path');
+
+module.exports = {
+	env: {
+		browser: true
+	},
+	extends: 'react-app',
+	globals: {
+		it: false,
+	},
+	parser: 'babel-eslint',
+	rules: {
+		'arrow-body-style': 'off',
+		'class-methods-use-this': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-cycle': 'off',
+		'import/named': 'off',
+		'import/no-named-as-default': 'off',
+		'import/no-named-as-default-member': 'off',
+		'indent': ['error', 'tab'],
+		'jsx-a11y/anchor-is-valid': 'off',
+		'jsx-a11y/label-has-associated-control': 'off',
+		'jsx-a11y/label-has-for': 'off',
+		'jsx-a11y/no-static-element-interactions': 'off',
+		'jsx-a11y/click-events-have-key-events': 'off',
+		'max-len': 'off',
+		'no-console': 'off',
+		'no-new': 'off',
+		'no-param-reassign': 'off',
+		'no-restricted-syntax': 'off',
+		'no-tabs': 'off',
+		'no-use-before-define': 'off',
+		'react/destructuring-assignment': 'off',
+		'react/jsx-indent': [true, 'tab'],
+		'react/jsx-indent-props': [2, 'tab'],
+		'react/jsx-one-expression-per-line': 'off',
+		'react/jsx-filename-extension': 'off',
+		'react/no-access-state-in-setstate': 'off',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				paths: [path.resolve(__dirname, 'src')],
+			},
+			// alias: {
+			// 	map: [
+			// 		['', './src'],
+			// 	],
+			// 	extensions: ['.js', '.jsx'],
+			// }
+		}
+	}
+};
